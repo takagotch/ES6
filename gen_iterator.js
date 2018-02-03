dfaca6ff9f80256f4c39d257f3775781f36865ea
takagotch <dyaccb@gmail.com>
2018-02-04 01:19:36 +0900

    for(let i =2; i <= Math.floor(Math.sqrt(value)); i++){ if(value % i === 0){ prime = false; break; }}return prime;

--- gen_iterator.js
+++ gen_iterator.js
@@ -1,3 +1,4 @@
+//gen_iterator
 class MyIterator {
   constructor(data){
     this.data = data;
@@ -9,6 +10,42 @@ class MyIterator {
       }
     };
   }
+}
+
+//generator
+function* myGenerator(){
+  yield '';
+  yield '';
+  yield '';
+}
+
+for(let t of myGenerator()){
+  console.log(t);
+}
+
+//
+function* genPrimes(){
+  let num = 2;
+  while(true){
+    if(isPrime(num)){ yeild num; }
+    num++;
+  }
+}
+
+function isPrime(value){
+  let prime = true;
+  for(let i = 2; i <= Math.floor(Math.sqrt(value)); i++){
+    if(value % i === 0){
+      prime = false;
+      break;
+    }
+  }
+  return prime;
+}
+
+for(let value of genPrimes()){
+  if(value > 100){ break; }
+  console.log(value);
 }
 
 
